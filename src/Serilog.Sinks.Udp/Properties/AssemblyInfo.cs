79098e127d32d06408b7eb6ec7ae45838fdcce5c
FantasticFiasco <mattias.kindborg@gmail.com>
2015-03-23 14:05:18 +0100

    Updated assembly information

--- src/Serilog.Sinks.Udp/Properties/AssemblyInfo.cs
+++ src/Serilog.Sinks.Udp/Properties/AssemblyInfo.cs
@@ -1,25 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Serilog.Sinks.Udp")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Serilog.Sinks.Udp")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e9336ce3-d210-47b7-ac20-7b5dd92c9761")]
+[assembly: AssemblyDescription("Serilog sink for UDP")]
+[assembly: AssemblyCopyright("Copyright © Serilog Contributors 2015")]
 
 // Version information for an assembly consists of the following four values:
 //
