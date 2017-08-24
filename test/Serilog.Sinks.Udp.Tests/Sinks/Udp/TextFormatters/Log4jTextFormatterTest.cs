7fda1a6de0220487c3bdca77716ad1a62ef5b87b
Mattias Kindborg <mattias.kindborg@gmail.com>
2017-08-24 20:02:10 +0200

    wip

---
+++ test/Serilog.Sinks.Udp.Tests/Sinks/Udp/TextFormatters/Log4jTextFormatterTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Serilog.Events;
+using Xunit;
+
+namespace Serilog.Sinks.Udp.TextFormatters
+{
+    public class Log4jTextFormatterTest
+    {
+        [Fact]
+        public void Logger()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Timestamp()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Theory]
+        [InlineData(LogEventLevel.Verbose, "TRACE")]
+        [InlineData(LogEventLevel.Debug, "DEBUG")]
+        [InlineData(LogEventLevel.Information, "INFO")]
+        [InlineData(LogEventLevel.Warning, "WARN")]
+        [InlineData(LogEventLevel.Error, "ERROR")]
+        [InlineData(LogEventLevel.Fatal, "FATAL")]
+        public void Level(LogEventLevel actual, string expected)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Thead()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Message()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Exception()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
