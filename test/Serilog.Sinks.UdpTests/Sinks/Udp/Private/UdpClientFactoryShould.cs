dd1dde9690607939b36272f06e208dda0b4a27ea
FantasticFiasco <mattias@kindb.org>
2025-12-23 11:10:33 +0100

    test: fix build error

--- test/Serilog.Sinks.UdpTests/Sinks/Udp/Private/UdpClientFactoryShould.cs
+++ test/Serilog.Sinks.UdpTests/Sinks/Udp/Private/UdpClientFactoryShould.cs
@@ -28,7 +28,7 @@ public class UdpClientFactoryShould : IDisposable
     [Theory]
     [InlineData("127.0.0.1", AddressFamily.InterNetwork)]
     [InlineData("::1", AddressFamily.InterNetworkV6)]
-    public async void SendPayload(string address, AddressFamily family)
+    public async Task SendPayload(string address, AddressFamily family)
     {
         // Arrange
         client = UdpClientFactory.Create(0, family, false);
